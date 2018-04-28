93dd1b1fef2c5b3b83e9fae6ecd92bb16b7c5e63
Jmeas <jellyes2@gmail.com>
2018-04-28 15:38:51 -0700

    Add tests for createResourceStore

--- test/create-resource-store.test.js
+++ test/create-resource-store.test.js
@@ -1,4 +1,5 @@
 import { createResourceStore } from '../src';
+import { warning } from '../src/utils/warning';
 
 describe('createResourceStore', () => {
   it('returns an object with the right shape', () => {
@@ -13,9 +14,124 @@ describe('createResourceStore', () => {
 
   describe('calling getState', () => {
     it('with no initial state returns an empty object', () => {
-      const store = createResourceStore();
+      const store = createResourceStore(null, {
+        schemas: {
+          books: {
+            idAttribute: 'bookId',
+          },
+        },
+      });
       const state = store.getState();
       expect(state).toEqual({});
+    });
+  });
+
+  describe('allows you to create and then retrieve a resource', () => {
+    const store = createResourceStore(null, {
+      schemas: {
+        books: {
+          idAttribute: 'bookId',
+        },
+      },
+    });
+
+    store.updateResources('books.resources', [{ bookId: 5 }]);
+
+    const resources = store.getResources('books', [5]);
+    expect(resources).toEqual([
+      {
+        bookId: 5,
+        resourceType: 'books',
+        attributes: {},
+        meta: {},
+        computedAttributes: {},
+      },
+    ]);
+  });
+
+  describe('allows you to create and then delete a resource', () => {
+    const store = createResourceStore(null, {
+      schemas: {
+        books: {
+          idAttribute: 'bookId',
+        },
+      },
+    });
+
+    store.updateResources('books.resources', [{ bookId: 5 }]);
+
+    expect(store.getResources('books', [5])).toEqual([
+      {
+        bookId: 5,
+        resourceType: 'books',
+        attributes: {},
+        meta: {},
+        computedAttributes: {},
+      },
+    ]);
+
+    store.deleteResources('books.resources', [{ bookId: 5 }]);
+
+    expect(store.getResources('books', [5])).toEqual([]);
+  });
+
+  describe('subscribing', () => {
+    it('warns if an invalid callback is supplied', () => {
+      const store = createResourceStore(null, {
+        schemas: {
+          books: {
+            idAttribute: 'bookId',
+          },
+        },
+      });
+
+      store.subscribe(false);
+
+      expect(warning).toHaveBeenCalledTimes(1);
+      expect(warning.mock.calls[0][1]).toEqual('LISTENER_INVALID_TYPE');
+      expect(warning.mock.calls[0][2]).toEqual('error');
+    });
+
+    it('calls the callback', () => {
+      const cb = jest.fn();
+
+      const store = createResourceStore(null, {
+        schemas: {
+          books: {
+            idAttribute: 'bookId',
+          },
+        },
+      });
+
+      store.subscribe(cb);
+      expect(cb).toHaveBeenCalledTimes(0);
+
+      store.updateResources('books.resources', [{ bookId: 5 }]);
+      expect(cb).toHaveBeenCalledTimes(1);
+    });
+
+    it('allows you to unsubscribe', () => {
+      const cb = jest.fn();
+
+      const store = createResourceStore(null, {
+        schemas: {
+          books: {
+            idAttribute: 'bookId',
+          },
+        },
+      });
+
+      const unsubscribe = store.subscribe(cb);
+      expect(cb).toHaveBeenCalledTimes(0);
+
+      store.updateResources('books.resources', [{ bookId: 5 }]);
+      expect(cb).toHaveBeenCalledTimes(1);
+
+      unsubscribe();
+      unsubscribe();
+
+      store.updateResources('books.resources', [{ bookId: 5 }]);
+      expect(cb).toHaveBeenCalledTimes(1);
     });
   });
 });
