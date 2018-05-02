5cc617cd777a641bd9af8387eb20ebb082871878
Jmeas <jellyes2@gmail.com>
2018-05-01 22:49:33 -0700

    Add new object-from-path util

---
+++ test/utils/object-from-path.test.js
@@ -0,0 +1,37 @@
+import objectFromPath from '../../src/utils/object-from-path';
+
+describe('objectFromPath', () => {
+  it('handles not passing anything', () => {
+    expect(objectFromPath()).toEqual({});
+  });
+
+  it('handles passing just a string', () => {
+    expect(objectFromPath('hello.friends')).toEqual({
+      hello: {
+        friends: undefined,
+      },
+    });
+  });
+
+  it('handles passing null as the change', () => {
+    expect(objectFromPath('hello.friends', null)).toEqual({
+      hello: {
+        friends: null,
+      },
+    });
+  });
+
+  it('handles passing an object as the change', () => {
+    expect(
+      objectFromPath('hello.friends', {
+        sandwiches: 'yum',
+      })
+    ).toEqual({
+      hello: {
+        friends: {
+          sandwiches: 'yum',
+        },
+      },
+    });
+  });
+});
