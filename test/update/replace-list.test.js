e098bcc0d52d4c6da67c0775699069e69518e029
Jmeas <jellyes2@gmail.com>
2018-05-04 10:57:29 -0700

    Dedupe pointers in list updates

--- test/update/replace-list.test.js
+++ test/update/replace-list.test.js
@@ -125,4 +125,68 @@ describe('update - replace list', function() {
     });
     expect(warning).toHaveBeenCalledTimes(0);
   });
+
+  it('should replace a list, ignoring duplicates', () => {
+    const newState = update({
+      state: this.state,
+      schemas: this.schemas,
+      changes: {
+        lists: {
+          favoriteBooks: [
+            { id: 10, resourceType: 'books' },
+            { id: 10, resourceType: 'books' },
+            { id: 10, resourceType: 'books' },
+          ],
+        },
+      },
+    });
+
+    expect(newState).toEqual({
+      resources: {
+        books: {
+          2: {
+            id: 2,
+            attributes: {
+              firstName: 'James',
+              lastName: 'Please',
+            },
+          },
+          5: {
+            id: 5,
+          },
+          10: {
+            id: 10,
+          },
+        },
+        authors: {
+          a: { id: 'a' },
+          b: { id: 'b' },
+        },
+      },
+      lists: {
+        favoriteBooks: [{ id: 10, resourceType: 'books' }],
+        newBooks: [
+          {
+            resourceType: 'books',
+            id: 1,
+          },
+          {
+            resourceType: 'books',
+            id: 5,
+          },
+          {
+            resourceType: 'books',
+            id: 10,
+          },
+        ],
+        things: [
+          {
+            id: 10,
+            resourceType: 'authors',
+          },
+        ],
+      },
+    });
+    expect(warning).toHaveBeenCalledTimes(0);
+  });
 });
