94412e29b40b47ec832a6388a1a97749dc5a9d0d
Jmeas <jellyes2@gmail.com>
2018-05-26 13:39:44 -0700

    Fix bugs in merge

--- src/utils/merge.js
+++ src/utils/merge.js
@@ -1,26 +1,54 @@
-import { isObject } from './identification';
+import { isObject, exists } from './identification';
 
+// This always returns an Object. Do not call this if you want to replace
+// an Object with a primitive.
 export default function merge(x, y, deep = false) {
-  if (!deep) {
-    return Object.assign({}, x, y);
+  // If y is not an Object, then it is a primitive. Primitives
+  // cannot be merged or cloned, so we return an empty object.
+  if (!isObject(y)) {
+    return Object.assign({}, x);
   }
 
-  // Start off by making a clone of `y`.
+  const isXObject = isObject(x);
+
+  // If y _is_ an Object, but we don't want to deep merge, then we can do a
+  // shallow clone.
+  if (!deep) {
+    // We only use x in our merge if it is an object, asrimitives cannot
+    // be merged.
+    const xToUse = isXObject ? x : {};
+
+    return Object.assign({}, xToUse, y);
+  }
+
+  // If we have made it this far, then we need to do a merge merge, and we know that
+  // at least y is an object.
+  // We begin by making a clone of `y`.
   let output = Object.assign({}, y);
 
-  // We can only deep merge when both x and y are objects
-  if (isObject(y) && isObject(x)) {
+  // We can perform a deep merge when both x and y are objects
+  if (isObject(x)) {
     // Loop through every key of x, and merge it with the y key.
     Object.keys(x).forEach(key => {
       // If the value is an object, then we either deep merge it with the
       // existing value, or we simply set it if it does not previously exist
       if (isObject(x[key])) {
-        output[key] = !(key in y) ? x[key] : merge(y[key], x[key], true);
+        const yValue = y[key];
+
+        if (!exists(yValue)) {
+          output[key] = x[key];
+        } else if (!isObject(yValue)) {
+          output[key] = yValue;
+        } else {
+          output[key] = merge(x[key], yValue, true);
+        }
       }
 
-      // If it is not an object, then there is nothing to deep merge, so we just set it.
+      // If this value from x is not an object, then it cannot be merged.
+      // What we must do in this situation is set the value as the value from y,
+      // if it exists. If it doesn't exist, then we keep the x value.
       else {
-        output[key] = x[key];
+        output[key] = exists(y[key]) ? y[key] : x[key];
       }
     });
   }
